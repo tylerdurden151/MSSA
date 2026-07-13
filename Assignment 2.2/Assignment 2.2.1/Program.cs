0491f5ba5c05fb46455d92774cc72817871e405f
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-13 08:56:28 -0700

    Created Assignment 2.2.1

---
+++ Assignment 2.2/Assignment 2.2.1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Assignment 2.2.1
+/* Dsign a class hierarch of your choice. Need only class files) 
+ * Pet ->
+ * Dog -> 
+ * Hound -> Beagle
+ * Terrier -> Welsh Terrier
+  Working -> Boxer */
+
