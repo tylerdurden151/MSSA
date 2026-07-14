bc3366a7c92edac995325acfc5e1a2f4b66d1749
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-14 09:20:27 -0700

    Added exception to StreamWriter

--- Assignment 2.3/Assignment 2.3.1/Program.cs
+++ Assignment 2.3/Assignment 2.3.1/Program.cs
@@ -6,17 +6,25 @@ using System;
 using System.IO;
 
 
-        // Create a text file and write basic details to it using user input.
-        using (StreamWriter sw = new StreamWriter("Assignment2.3.1_text.txt"))
-        {
-            Console.Write("Name: ");
-            sw.WriteLine("Name: " + Console.ReadLine());
-            Console.Write("Age: ");
-            sw.WriteLine("Age: " + Console.ReadLine());
-            Console.Write("Address: ");
-            sw.WriteLine("Address: " + Console.ReadLine());
-        }
-
+try
+{
+    // Create a text file and write basic details to it using user input.
+    using (StreamWriter sw = new StreamWriter("Assignment2.3.1_text.txt"))
+    {
+        Console.Write("Name: ");
+        sw.WriteLine("Name: " + Console.ReadLine());
+        Console.Write("Age: ");
+        sw.WriteLine("Age: " + Console.ReadLine());
+        Console.Write("Address: ");
+        sw.WriteLine("Address: " + Console.ReadLine());
+    }
+}
+catch (Exception e)
+{ 
+    // Let the user know what went wrong.
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
         try
         {
             // Read the details from the same file and print on console.
