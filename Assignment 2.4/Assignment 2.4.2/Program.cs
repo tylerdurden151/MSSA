b4776387793c9107e07e897eea80e566ecfcaa20
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-15 08:38:43 -0700

    Completed Draft of Assignment 2.4.2.

--- Assignment 2.4/Assignment 2.4.2/Program.cs
+++ Assignment 2.4/Assignment 2.4.2/Program.cs
@@ -13,3 +13,34 @@ Expected Output :
 
 The 2nd Number is the greatest among three
  */
+
+Console.WriteLine("Please input the first number.");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please input the second number.");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please input the third number.");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int currentLargest = num1;
+for (int i = 0; i < 3; i++)
+{
+    if (i == 0)
+    {
+        currentLargest = num1;
+    }
+    else if (i == 1)
+    {
+        if (num2 > currentLargest)
+        {
+            currentLargest = num2;
+        }
+    }
+    else if (i == 2)
+    {
+        if (num3 > currentLargest)
+        {
+            currentLargest = num3;
+        }
+    }
+}
+Console.WriteLine($"The largest number is: {currentLargest}");
