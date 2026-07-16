c19a2f97671284427126efd41c1167e0823848cb
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-16 13:12:44 -0700

    added Checkbox

--- Sandbox/Tutorial 1/Form1.Designer.cs
+++ Sandbox/Tutorial 1/Form1.Designer.cs
@@ -29,6 +29,10 @@ partial class Form1
     private void InitializeComponent()
     {
         tableLayoutPanel1 = new TableLayoutPanel();
+        pictureBox1 = new PictureBox();
+        checkBox1 = new CheckBox();
+        tableLayoutPanel1.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
         SuspendLayout();
         // 
         // tableLayoutPanel1
@@ -36,6 +40,8 @@ partial class Form1
         tableLayoutPanel1.ColumnCount = 2;
         tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
         tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
+        tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+        tableLayoutPanel1.Controls.Add(checkBox1, 0, 1);
         tableLayoutPanel1.Dock = DockStyle.Fill;
         tableLayoutPanel1.Location = new Point(0, 0);
         tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -45,6 +51,27 @@ partial class Form1
         tableLayoutPanel1.Size = new Size(971, 817);
         tableLayoutPanel1.TabIndex = 0;
         // 
+        // pictureBox1
+        // 
+        pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+        tableLayoutPanel1.SetColumnSpan(pictureBox1, 2);
+        pictureBox1.Dock = DockStyle.Fill;
+        pictureBox1.Location = new Point(3, 3);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(965, 729);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // checkBox1
+        // 
+        checkBox1.AutoSize = true;
+        checkBox1.Location = new Point(3, 738);
+        checkBox1.Name = "checkBox1";
+        checkBox1.Size = new Size(92, 29);
+        checkBox1.TabIndex = 1;
+        checkBox1.Text = "Stretch";
+        checkBox1.UseVisualStyleBackColor = true;
+        // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(10F, 25F);
@@ -53,10 +80,15 @@ partial class Form1
         Controls.Add(tableLayoutPanel1);
         Name = "Form1";
         Text = "Picture Viewer";
+        tableLayoutPanel1.ResumeLayout(false);
+        tableLayoutPanel1.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
         ResumeLayout(false);
     }
 
     #endregion
 
     private TableLayoutPanel tableLayoutPanel1;
+    private PictureBox pictureBox1;
+    private CheckBox checkBox1;
 }
