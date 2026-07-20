0f14baef22a0fd4777dce723f7a8dfd637224199
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-20 09:23:35 -0700

    Solved method, next need to fix output with string and space

--- Assignments 3.1/Assignment 3.1.3/Program.cs
+++ Assignments 3.1/Assignment 3.1.3/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ 3. Write a program in C# Sharp to create a function to input a string 
+and count number of spaces are in the string.
+Test Data :
+Please input a string : This is a test string.
+Expected Output :
+"This is a test string." contains 4 spaces
+ 
+ */
+
+using System.Text;
+
+
+var result = SpacesInString("This is a test string.");
+Console.WriteLine(result);
+
+int SpacesInString(string value) { 
+    
+    char [] chars = value.ToCharArray();
+    int count = 0;
+    if (chars == null)
+    {
+        return 0;
+    }
+
+    foreach (char c in chars)
+    {
+        if (c == ' ')
+        {
+            count++;
+        }
+    }
+   return count; 
+}
