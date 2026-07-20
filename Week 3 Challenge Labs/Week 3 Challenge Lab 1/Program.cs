6f956d5ff785958fe74baab5465dade2756bd0b5
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-20 12:30:35 -0700

    running program, testing

--- Week 3 Challenge Labs/Week 3 Challenge Lab 1/Program.cs
+++ Week 3 Challenge Labs/Week 3 Challenge Lab 1/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ 1. Given a string, write a method that checks 
+
+if it is a palindrome (is read the same backward as forward). 
+
+Assume that string may consist only of lower-case letters.
+
+Expected input and output
+
+IsPalindrome("eye") → true
+
+IsPalindrome("home") → false
+ 
+ */
+
+var result = IsPalindrome("stoots");
+Console.WriteLine(result);
+
+bool IsPalindrome(string p) {
+    char [] chars = p.ToCharArray();
+    int left = 0;
+    int right = chars.Length - 1;
+
+    while (left < right)
+    {
+        char temp = chars[left];
+        chars[left] = chars[right];
+        chars[right] = temp;
+        right--;
+        left++;
+    }
+    if (chars.ToString() == p)
+    {
+        return true;
+    }
+    return false;
+}
