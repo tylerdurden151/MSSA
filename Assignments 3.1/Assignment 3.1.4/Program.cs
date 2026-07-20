57609c6f5a54467f0dfeb970c6753b0988056764
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-20 10:05:57 -0700

    Working problem

--- Assignments 3.1/Assignment 3.1.4/Program.cs
+++ Assignments 3.1/Assignment 3.1.4/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ 4. Write a function which takes an array as input 
+and finds the first occurrence of 2 consecutive 1s and changes their value to 0.
+
+For e.g: Input : [0,2,1,1,9,1,1]
+
+Output: [0,2,0,0,9,1,1]
+ */
+
+public class Solution
+{
+    public int[] ConsecutiveOnes(int[] nums)
+    {
+        int left = 0;
+        int curr = 0;
+        int[] result = new int[nums.Length];
+
+        for (int right = 0; right < nums.Length; right++) {
+            if (nums[right] == 1)
+            {
+                curr++;
+            }
+            while (curr > 1)
+            {
+                if (nums[left] == 1)
+                {
+                    curr--;
+                }
+                left++;
+            }
+            result[curr] = curr;
+
+
+}
