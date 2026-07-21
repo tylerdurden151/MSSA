a2ff7604fdd118fdc16a7b78a3f210fd6e4be999
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-21 14:25:38 -0700

    Completed the assignment

--- Week 3 Challenge Labs/Week 3 Challenge Lab 4/Program.cs
+++ Week 3 Challenge Labs/Week 3 Challenge Lab 4/Program.cs
@@ -1,11 +1,11 @@
 ﻿/*
  4. You are given a string s consisting only of uppercase English letters.
 
-You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
-
+You can apply some operations to this string where, in one operation, you can remove 
+any occurrence of one of the substrings "AB" or "CD" from s.
 Return the minimum possible length of the resulting string that you can obtain.
-
-Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+Note that the string concatenates after removing the substring and
+could produce new "AB" or "CD" substrings.
 
 **Hint : Use Replace method of string.
 
@@ -36,3 +36,26 @@ Output: 5
 Explanation: We cannot do any operations on the string so the length remains the same.
  
  */
+
+Solution solution = new Solution("ABFCACDB");
+Console.WriteLine(solution.MinLength());
+
+
+public class Solution
+{
+    private string _s = string.Empty;
+    public string S { get { return _s; } set { _s = value; } }
+    public Solution(string s)
+    {
+        S = s;
+    }
+    public int MinLength()
+    {
+        // Keep replacing "AB" and "CD" until no more replacements can be made
+        while (S.Contains("AB") || S.Contains("CD"))
+        {
+            S = S.Replace("AB", "").Replace("CD", "");
+        }
+        return S.Length;
+    }
+}
