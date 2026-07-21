2ff1f24f9288b987daeaacc1ab815b04618e17dd
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-21 09:15:31 -0700

    Modified for alginment

--- Assignments 3.2/Assignment 3.2.1/Program.cs
+++ Assignments 3.2/Assignment 3.2.1/Program.cs
@@ -9,7 +9,7 @@ e. g:
 
 | 1 | 4 | 6 |
  */
-int[] value = { 2, 3, 4, 1, 4, 6 };
+int[] value = { 2, 3, 44, 1, 4, 6 };
 
 TwoDArray t = new TwoDArray(value);
 t.DisplayTwoD();
@@ -26,6 +26,17 @@ public class TwoDArray
 
     public void DisplayTwoD()
     {
+        //Get the length of the array and storing the width of the digits.
+        int width = 0;
+        foreach (int n in Number)
+        {
+            int len = n.ToString().Length;
+            if (len > width)
+            {
+                width = len;
+            }
+        }
+
         int count = 0;
         foreach (int n in Number)
         {
@@ -34,7 +45,7 @@ public class TwoDArray
             {
                 Console.Write("|");
             }
-            Console.Write($" {n} |");
+            Console.Write($" {n.ToString().PadLeft(width)} |");
             count++;
             //Break line up to 3 values
             if (count % 3 == 0)
