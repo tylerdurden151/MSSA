b5806df542bbde0d91d23dad11dc67e0ad4df873
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-21 08:45:53 -0700

    Completed the Assignment

--- Assignments 3.2/Assignment 3.2.1/Program.cs
+++ Assignments 3.2/Assignment 3.2.1/Program.cs
@@ -9,3 +9,37 @@ e. g:
 
 | 1 | 4 | 6 |
  */
+int[] value = { 2, 3, 4, 1, 4, 6 };
+
+TwoDArray t = new TwoDArray(value);
+t.DisplayTwoD();
+
+public class TwoDArray
+{
+    private int[] _number = Array.Empty<int>();
+    public int[] Number { get { return _number; } set { _number = value; } }
+
+    public TwoDArray(int[] nums)
+    {
+        Number = nums;
+    }
+
+    public void DisplayTwoD()
+    {
+        int count = 0;
+        foreach (int n in Number)
+        {
+            if (count % 3 == 0)
+            {
+                Console.Write("|");
+            }
+            Console.Write($" {n} |");
+            count++;
+            //Break line up to 3 values
+            if (count % 3 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+} 
