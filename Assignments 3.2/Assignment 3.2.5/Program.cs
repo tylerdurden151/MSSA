bbbf164c1206ca3d5b5b0f133889ab6fb190fd7b
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-21 11:24:38 -0700

    Completed Assignment

--- Assignments 3.2/Assignment 3.2.5/Program.cs
+++ Assignments 3.2/Assignment 3.2.5/Program.cs
@@ -13,3 +13,36 @@ Notes
 
 If the item is not present, return -1.
  */
+Solution s = new Solution(new int[] { 1, 5, 3 }, 5);
+Solution s1 = new Solution(new int[] { 9, 8, 3 }, 3);
+Solution s2 = new Solution(new int[] { 1, 2, 3 }, 4);
+Console.WriteLine(s.Search());    // 1  (5 is at index 1)
+Console.WriteLine(s1.Search());   // 2  (3 is at index 2)
+Console.WriteLine(s2.Search());   // -1 (4 not present)
+
+
+public class Solution
+{
+    private int[] _arr = Array.Empty<int>();
+    private int _k;
+    public int[] Arr { get { return _arr; } set { _arr = value; } }
+    public int K { get { return _k; } set { _k = value; } }
+
+    public Solution(int[] arr, int k)
+    {
+        Arr = arr;
+        K = k;
+    }
+
+    public int Search()
+    {
+        for (int i = 0; i < Arr.Length; i++)
+        {
+            if (Arr[i] == K)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
