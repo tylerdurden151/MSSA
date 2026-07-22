87040b21141130a81cce4699b36657f3f2f34e9b
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-22 07:49:29 -0700

    Created project folder for Assignment

---
+++ Assignments 3.3/Assignment 3.3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+ Windows form assignments
+
+Use different controls on windows forms and change their properties (do not submit this assignment)
+Create a class: Student with fields like studid, firstname, lastname, address,monthofadmission,grade to hold character.
+Create a list of students and perform following operations in windows application:
+
+add new records,
+delete record and
+display them in grid.
+Monthofadmission should be enum.
+ */
