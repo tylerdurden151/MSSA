83ea00dd2a75b74cf27cf50d76e89daa679aa93c
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-22 09:28:17 -0700

    Added properties

--- Assignments 3.3/Assignment 3.3.1/Class1.cs
+++ Assignments 3.3/Assignment 3.3.1/Class1.cs
@@ -6,11 +6,13 @@ namespace Assignment_3._3._1
 {
     internal class Student
     {
-        public int StudId { get; set; }
+        private int _studId;
+        private char _grade;
+        public int StudId { get { return _studId; } set { _studId = value; } }
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
         public string Address { get; set; } = "";
         public string MonthOfAdmission { get; set; } = "";
-        public char Grade { get; set; }
+        public char Grade { get { return _grade; } set { _grade = value; }  }
     }
 }
