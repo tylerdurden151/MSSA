8d55c1a371031167e6443a1043ede41143d4016f
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-27 09:33:00 -0700

    Created projuect file

---
+++ Assignments 4.1/Assignments 4.1/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Design a personal phone /address book using Dictionary collection. 
+ 
+Write a person class to store details of person like : FirstName , LastName , Mobile Phone , Work Phone and Address.
+
+Provide a grid to display all, add more, delete ( with confirmation message) and search by name.
+
+Search should show details of person searched. You may use the key as name of person.
+ */
