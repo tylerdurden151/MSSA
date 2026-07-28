86bfce38d54bebbd86c3b9aa607fa3c956e18269
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-28 08:56:43 -0700

    Create Project

---
+++ Assignments 4.1.2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Assignments_4._1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Assignments 4.1.2/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignments_4._1._2
+{
+    internal interface ICalculator
+    {
+        double Add(double x, double y);
+        double Subtract(double x, double y);
+        double Multiply(double x, double y);    
+        double Divide(double x, double y);
+
+
+    }
+}
