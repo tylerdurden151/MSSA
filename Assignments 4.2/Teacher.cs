1319c8e4745885751dac1df20a94d5073cdaf7cc
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-29 11:18:08 -0700

    Teacher class created

---
+++ Assignments 4.2/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignments_4._2
+{
+    internal class Teacher : User
+    {
+        public string Subject { get; set; }
+
+        public Teacher(string userName, string subject)
+        {
+            UserName = userName;
+            Subject = subject;
+        }
+        public override bool SetPassword(string password)
+        {
+            // Example implementation - you can customize this as needed
+            return password.Length >= 8;
+        }
+    }
+}
--- Assignments 4.2/User.cs
+++ Assignments 4.2/User.cs
@@ -8,10 +8,13 @@ namespace Assignments_4._2
     {
         public string UserName { get; set; }
         protected string _password = Guid.NewGuid().ToString(); // Generate a random password by default
-        public string Password { set { if (!SetPassord(value))
+        public string Password { 
+            set {
+                if (!SetPassword(value))
                 {
                 throw  new ArgumentException("Password not complex enough", "Password");
                 }
+                _password = value;
             } 
         }
         public bool VerifyPassword(string password)
@@ -19,7 +22,7 @@ namespace Assignments_4._2
             return (String.Compare(_password, password) == 0);
         }
 
-        public abstract bool SetPassord(string password);
+        public abstract bool SetPassword(string password);
 
     }
 }
