3424be59731e0aca7dc4e973c8a2f8e509372fd9
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-29 11:07:13 -0700

    Created User class

---
+++ Assignments 4.2/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignments_4._2
+{
+    internal abstract class User
+    {
+        public string UserName { get; set; }
+        protected string _password = Guid.NewGuid().ToString(); // Generate a random password by default
+        public string Password { set { if (!SetPassord(value))
+                {
+                throw  new ArgumentException("Password not complex enough", "Password");
+                }
+            } 
+        }
+        public bool VerifyPassword(string password)
+        {
+            return (String.Compare(_password, password) == 0);
+        }
+
+        public abstract bool SetPassord(string password);
+
+    }
+}
