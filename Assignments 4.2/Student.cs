cda962294fbd1fd66be03dddd494d0c273038498
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-29 11:22:25 -0700

    Student class created

---
+++ Assignments 4.2/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignments_4._2
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public double GPA { get; set; }
+        public Student(int studentId, string studentName, double gpa)
+        {
+            StudentId = studentId;
+            StudentName = studentName;
+            GPA = gpa;
+        }
+    }
+}
