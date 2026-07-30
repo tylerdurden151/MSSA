b30d7bb9afc213c2cbf9cc810199fd5506db710e
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-30 11:15:12 -0700

    Completed Assignment

--- Assignments 4.3/Assignment 4.3.3/Program.cs
+++ Assignments 4.3/Assignment 4.3.3/Program.cs
@@ -10,3 +10,68 @@ Expected Output :
 The unique elements found in the array are :
 5
 */
+
+Solution.Run();
+
+public class Solution
+{
+    public static void Run()
+    {
+        Console.WriteLine("Enter the number of elements to be stored in the array:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine($"Input {n} elements in the array:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"element - {i} : ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        UniqueElements(arr);
+    }
+
+    public static void UniqueElements(int[] arr)
+    {
+        // Count occurrences of each element
+        Dictionary<int, int> elementCount = new Dictionary<int, int>();
+        foreach (var item in arr)
+        {
+            if (elementCount.ContainsKey(item))
+            {
+                elementCount[item]++;
+            }
+            else
+            {
+                elementCount[item] = 1;
+            }
+        }
+
+        // Get elements that appear only once
+        HashSet<int> uniqueElements = new HashSet<int>();
+        foreach (var kvp in elementCount)
+        {
+            if (kvp.Value == 1)
+            {
+                uniqueElements.Add(kvp.Key);
+            }
+        }
+
+        PrintUniqueElements(uniqueElements);
+
+    }
+
+    public static void PrintUniqueElements(HashSet<int> uniqueElements)
+    {
+        Console.WriteLine("The unique elements found in the array are :");
+        if (uniqueElements.Count > 0)
+        {
+            foreach (var item in uniqueElements)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No unique elements found.");
+        }
+    }
+}
