74ccb152aa9b00390f7ac38089c709c44225ebd5
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-30 09:15:39 -0700

    Completed Lab

--- Week 4 Challenge Labs/Week 4 Challenge Lab 3/Program.cs
+++ Week 4 Challenge Labs/Week 4 Challenge Lab 3/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ */
+
+char [] a = new char[]{'a', 'b', 'c', 'd', 'e'};
+Solution.ReverseArray(a);  
+
+
+class Solution
+{
+    public static void ReverseArray(char[] a)           
+    {
+        Console.WriteLine(string.Join(" ", a));
+
+        //declare pointer variables
+        int left = 0;
+        int right = a.Length - 1;
+
+        //compare left and
+        //right pointer values and swap them
+        while (left < right)
+        {
+            //swap values
+            char temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+            //increment left pointer and decrement right pointer
+            left++;
+            right--;
+        }
+        Console.WriteLine(string.Join(" ", a));
+
+    }
+}
+
+
