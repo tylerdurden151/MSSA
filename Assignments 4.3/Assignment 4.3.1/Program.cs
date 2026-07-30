dae5e6586bfd3e2e0b18ba104fbc9e269e8a2c53
Tim Eckart <tylerdurden_151@yahoo.com>
2026-07-30 09:53:21 -0700

    Created the projects

---
+++ Assignments 4.3/Assignment 4.3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+ Write a program in C# Sharp to calculate and print the Electricity bill of a given customer. 
+The customer id., name and unit consumed by the user should be taken from the keyboard and display the total amount to pay to the customer. 
+The charge are as follows: (you may change the charge sheet values)
+
+example:
+If bill exceeds $ 400 then a surcharge of 15% will be charged.
+
+Test Data :
+1001
+James
+800
+Expected Output :
+Customer IDNO :1001
+Customer Name :James
+unit Consumed :800
+Amount Charges @$ 2.00 per unit : 1600.00
+Surcharge Amount : 240.00
+Net Amount Paid By the Customer : 1840.00
+ */
---
+++ Assignments 4.3/Assignment 4.3.3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+3. Write a program in C# Sharp to print all unique elements in an array.
+Test Data :
+Input the number of elements to be stored in the array :3
+Input 3 elements in the array :
+element - 0 : 1
+element - 1 : 5
+element - 2 : 1
+Expected Output :
+The unique elements found in the array are :
+5
+*/
