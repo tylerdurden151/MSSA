ce1ff7b79bf92a6eae8f88c75e1f5f25d45d9913
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-03 10:16:56 -0700

    Completed Assignment

--- Assignments 5.1/Assignment 5.1.2/Program.cs
+++ Assignments 5.1/Assignment 5.1.2/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ */
+try
+{
+    Console.WriteLine(Solution.Sum(123));
+    Console.WriteLine(Solution.Sum(0));
+    Console.WriteLine(Solution.Sum(5342));
+    Console.WriteLine(Solution.Sum(-5));  // This will throw
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+
+public class Solution
+{
+    public static int Sum(int nums)
+    {
+        if (nums < 0)
+        {
+            throw new ArgumentException("Input must be a non-negative integer");
+        }
+
+        int sum = 0;
+        while (nums > 0)
+        {
+            int digit = nums % 10;
+            nums /= 10;
+            sum += digit;
+        }
+        return sum;
+    }
+}
