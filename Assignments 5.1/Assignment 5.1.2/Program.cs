b9a7eb1783ed1d5466301e53739dc2d888cac83b
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-03 10:18:03 -0700

    Added Headers

--- Assignments 5.1/Assignment 5.1.2/Program.cs
+++ Assignments 5.1/Assignment 5.1.2/Program.cs
@@ -1,4 +1,13 @@
 ﻿/*
+ * 2. Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+
+Test Data :
+
+Enter a number: 1234
+
+Expected Output :
+
+The sum of the digits of the number 1234 is : 10
  */
 try
 {
