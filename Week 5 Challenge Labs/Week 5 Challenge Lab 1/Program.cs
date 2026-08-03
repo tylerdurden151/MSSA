cafa9379d0ca626165daee9251bf4d9e436a4721
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-03 14:27:36 -0700

    Added to Solutions

--- Week 5 Challenge Labs/Week 5 Challenge Lab 1/Program.cs
+++ Week 5 Challenge Labs/Week 5 Challenge Lab 1/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ */
+int[] nums = { 4, 1, 2, 1, 2 };
+int[] nums2 = { 2, 2, 1 };
+
+Solution s = new Solution();
+Console.WriteLine(s.FindSingleNumber(nums));
+Console.WriteLine(s.FindSingleNumber(nums2));
+
+Solution2 s2 = new Solution2();
+Console.WriteLine(s2.FindSingleNumber(nums));
+Console.WriteLine(s2.FindSingleNumber(nums2));
+
+
+public class  Solution
+{
+    //Looked this one up  bitwise XOR
+    public int FindSingleNumber(int[] nums)
+    {
+        int result = 0;
+        foreach (int num in nums)
+        {
+            result ^= num;
+        }
+        return result;
+    }
+}
+
+public class Solution2
+{
+    public int FindSingleNumber(int[] nums)
+    {
+        // Same method as the Contains Duplicate prble problem,
+        // but instead of returning true when we find a duplicate,
+        // we remove it from the set.
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int n in nums)
+        {
+            if (!seen.Add(n)) {
+                // was already there -> cancel the pair
+                seen.Remove(n);   
+            }
+        } 
+        return seen.Single();
+    }
+}
