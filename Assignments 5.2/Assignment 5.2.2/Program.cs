eb83c0af1c90f66da9508a797d08f0071b22b969
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-04 10:57:06 -0700

    Completed Assignment

--- Assignments 5.2/Assignment 5.2.2/Program.cs
+++ Assignments 5.2/Assignment 5.2.2/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ * 2. Write a program in C# Sharp to print the first n natural number using recursion.
+Test Data :
+How many numbers to print : 10
+Expected Output :
+1 2 3 4 5 6 7 8 9 10
+ */
+Solution s = new Solution();
+s.Num(10); 
+
+public class Solution
+{
+    public void Num(int num)
+    {
+        
+        if (num <= 0)
+        {
+            return;
+        }
+        Num(num - 1);
+        Console.Write(num + " ");
+        
+    }    
+}
+
