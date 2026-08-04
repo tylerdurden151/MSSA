d93b03badd6b74e7f2bd41796b4fa4398cc13055
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-04 10:26:23 -0700

    Completed Assignment

--- Assignments 5.2/Assignment 5.2.1/Program.cs
+++ Assignments 5.2/Assignment 5.2.1/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ * Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+ */
+Solution s = new Solution();
+Console.WriteLine(s.Word("Hello World")); // Output: 5")
+Console.WriteLine(s.Word("   fly me   to   the moon  ")); // Output: 4")
+Console.WriteLine(s.Word("luffy is still joyboy")); // Output: 6")
+public class Solution
+{
+    public int Word(string s)
+    {
+
+        string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        string lastWord = words[words.Length - 1];
+        return lastWord.Length;
+
+
+    }
+}
