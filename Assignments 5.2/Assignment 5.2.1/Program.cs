05f6bad952dd97073a2e149e34ceb71cee369879
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-04 10:37:03 -0700

    Added better version

--- Assignments 5.2/Assignment 5.2.1/Program.cs
+++ Assignments 5.2/Assignment 5.2.1/Program.cs
@@ -17,6 +17,12 @@ Solution s = new Solution();
 Console.WriteLine(s.Word("Hello World")); // Output: 5")
 Console.WriteLine(s.Word("   fly me   to   the moon  ")); // Output: 4")
 Console.WriteLine(s.Word("luffy is still joyboy")); // Output: 6")
+
+Solution2 s2 = new Solution2();
+Console.WriteLine(s2.LengthOfLastWord("Hello World")); // Output: 5")
+Console.WriteLine(s2.LengthOfLastWord("   fly me   to   the moon  ")); // Output: 4")
+Console.WriteLine(s2.LengthOfLastWord("luffy is still joyboy")); // Output: 6")
+
 public class Solution
 {
     public int Word(string s)
@@ -26,6 +32,32 @@ public class Solution
         string lastWord = words[words.Length - 1];
         return lastWord.Length;
 
+
+    }
+}
+
+// This solution is more efficient than the first one because it doesn't create an array of words,
+// it just iterates through the string once and counts the length of the last word.
+public class Solution2
+{
+    public int LengthOfLastWord(string s)
+    {
+
+        int i = s.Length - 1;
+
+        while (i >= 0 && s[i] == ' ')
+        {
+            i--;
+        }
+
+        int end = i;
+
+        while (i >= 0 && s[i] != ' ')
+        {
+            i--;
+        }
+
+        return (end - i);
 
     }
 }
