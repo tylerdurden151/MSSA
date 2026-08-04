331212df003e081bc950382fb97f7a1aba4d72c4
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-04 15:23:12 -0700

    Assignment Complete

--- Week 5 Challenge Labs/Week 5 Challenge Lab 2/Program.cs
+++ Week 5 Challenge Labs/Week 5 Challenge Lab 2/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ */
+
+Solution s = new Solution();
+Console.WriteLine(s.MissingNumber(new int[] { 3, 0, 1 }));
+Console.WriteLine(s.MissingNumber(new int[] { 0, 1 }));
+Console.WriteLine(s.MissingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }));
+
+class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        foreach (int num in nums)
+        {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+}
