e41f2dcb975fef838b8f688dcb2ac4eac36a0899
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-04 11:05:31 -0700

    Completed Assignment

--- Assignments 5.2/Assignment 5.2.3/Program.cs
+++ Assignments 5.2/Assignment 5.2.3/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Solution s = new Solution();
+s.Num(10); // Output: 10 9 8 7 6 5 4 3 2 1
+
+public class Solution
+{
+    public void Num(int num)
+    {
+
+        if (num <= 0)
+        {
+            return;
+        }
+        Console.Write(num + " ");
+        Num(num - 1);
+
+    }
+}
