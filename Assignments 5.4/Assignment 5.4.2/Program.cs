ca5fde7e833dd98600444650b665666b3db250b4
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-06 11:11:10 -0700

    Added comments

--- Assignments 5.4/Assignment 5.4.2/Program.cs
+++ Assignments 5.4/Assignment 5.4.2/Program.cs
@@ -1,4 +1,17 @@
-﻿/**/
+﻿/*Write a C# Sharp program to find the sum of the right diagonals of a matrix.
+Test Data :
+Input the size of the square matrix : 2
+Input elements in the first matrix :
+element - [0],[0] : 1
+element - [0],[1] : 2
+element - [1],[0] : 3
+element - [1],[1] : 4
+Expected Output :
+The matrix is :
+1 2
+3 4
+Addition of the right Diagonal elements is :5
+*/
 Solution s = new Solution();
 s.RunMenu();
 
@@ -51,6 +64,8 @@ public class Solution
         }
         return matrix;
     }
+
+  
 
     //Displays the matrix in a formatted way
     private void DisplayMatrix(int[,] matrix)
