4c7c91692c59b261f76f32ad69200472d36042fa
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-11 10:08:55 -0700

    Completed Assignment

--- 6. Week 6/Assignments 6.2/Assignment 6.2.2/Program.cs
+++ 6. Week 6/Assignments 6.2/Assignment 6.2.2/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/**/
+Solution s = new Solution();
+ int[] result = s.ProductofArray(new int[] { 1, 2, 3, 4 });
+Console.WriteLine(string.Join(", ", result));
+
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        // Calculate the product of all elements to the left of each element
+        result[0] = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // Calculate the product of all elements to the right of each element
+        int rightProduct = 1;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
