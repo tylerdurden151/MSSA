51b7fbaec34b9ea863778d89311135fc1a17b136
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-17 11:20:25 -0700

    Completed Assignment

---
+++ 7. Week 7/Assignments 7.1/Assignment 7.1.2/Program.cs
@@ -0,0 +1,31 @@
+﻿/**/
+Solution s = new Solution();
+string word1 = "abc";
+string word2 = "pqr";
+Console.WriteLine(s.MergeAlternately(word1,word2));
+
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        //strings are immutable
+        char[] result = new char[word1.Length + word2.Length];
+        int word1pos = 0;
+        int word2pos = 0;
+        int pos = 0;
+
+        while (word1pos < word1.Length || word2pos < word2.Length) {
+            if (word1pos < word1.Length)
+            {
+                result[pos++] = word1[word1pos++];
+            }
+            if (word2pos < word2.Length)
+            {
+                result[pos++] = word2[word2pos++];
+            }
+        
+        }
+        return new string(result);
+    }
+ }
