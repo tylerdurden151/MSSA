7e6436bac72c90189e9f96671da8f787f417d3e1
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-18 09:24:04 -0700

    Completed Assignment

--- 7. Week 7/Assignments 7.2/Assignment 7.2.1/Program.cs
+++ 7. Week 7/Assignments 7.2/Assignment 7.2.1/Program.cs
@@ -1,3 +1,74 @@
 ﻿/**/
+Solution s = new Solution();
+s.UserInputArray();
 
+public class Solution
+{
+    public void UserInputArray()
+    {
+        Console.WriteLine("Enter the size of the array: ");
+        Console.WriteLine();
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[size];
+        Console.WriteLine();
 
+        Console.WriteLine("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.WriteLine("The elements of the array are: ");
+        Console.WriteLine(string.Join(", ", arr));
+        int[] sortedArr = SortMethod(arr);
+        Console.WriteLine("The sorted elements of the array are: ");
+        Console.WriteLine(string.Join(", ", sortedArr));
+    }
+
+    public int[] SortMethod(int[] arr)
+    {
+        int[] temp = new int[arr.Length];
+        void Merger(int left, int right)
+        {
+            if (left >= right) {
+                return;
+            }
+            int mid = (left + right) / 2;
+            Merger(left, mid);
+            Merger(mid + 1, right);
+            MergeHelper(left, mid, right);
+        }
+        void MergeHelper(int left, int mid, int right)
+        {
+            int i = left;
+            int j = mid + 1;
+            int k = left;
+            while (i <= mid && j <= right)
+            {
+                if (arr[i] <= arr[j])
+                {
+                    temp[k++] = arr[i++];
+                }
+                else
+                {
+                    temp[k++] = arr[j++];
+                }
+            }
+            while (i <= mid)
+            {
+                temp[k++] = arr[i++];
+            }
+            while (j <= right)
+            {
+                temp[k++] = arr[j++];
+            }
+            for (int l = left; l <= right; l++)
+            {
+                arr[l] = temp[l];
+            }
+        }
+        Merger(0, arr.Length - 1);
+        return arr;
+    }
+
+}
