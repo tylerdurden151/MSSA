ad5a62c09c8a28817b7384208320d6e561981b22
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-18 10:00:51 -0700

    Added comment header

--- 7. Week 7/Assignments 7.2/Assignment 7.2.1/Program.cs
+++ 7. Week 7/Assignments 7.2/Assignment 7.2.1/Program.cs
@@ -1,4 +1,22 @@
-﻿/**/
+﻿/*
+ Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+Example 1:
+
+Input: s = "hello"
+
+Output: "holle"
+
+Example 2:
+
+Input: s = "avacado"
+
+Output: "ovacada"
+
+Example 3: “intelligent”-à   i , e, i, e
+
+Output: entillegint*/
 Solution s = new Solution();
 s.UserInputArray();
 
