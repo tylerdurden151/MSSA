7faa3d2774ab3d48fd4b6b4601ea96274e8a6de1
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-19 09:56:21 -0700

    Created TreeNode Class

--- 7. Week 7/Assignments 7.3/Assignment 7.3.1/Program.cs
+++ 7. Week 7/Assignments 7.3/Assignment 7.3.1/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/**/
+public class Solution
+{
+
+    public TreeNode BST(TreeNode root, int val)
+    {
+
+    }
+}
+
+
+public class TreeNode<T> where T : IComparable<T>
+{
+    //Added interface constraint to ensure that the data type T can be compared, which is necessary for binary search tree operations.
+
+    public T Data { get; set; }
+
+    public TreeNode<T>? Parent { get; set; }
+
+    public TreeNode<T>? Left { get; set; }
+
+    public TreeNode<T>? Right { get; set; }
+
+    public TreeNode(T data)
+    {
+        Data = data;
+    }
+
+}
