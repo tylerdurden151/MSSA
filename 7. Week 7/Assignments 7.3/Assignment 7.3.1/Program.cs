343889e71dbece13314073e6a17497991d99b742
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-19 10:14:22 -0700

    Assignment Completed

--- 7. Week 7/Assignments 7.3/Assignment 7.3.1/Program.cs
+++ 7. Week 7/Assignments 7.3/Assignment 7.3.1/Program.cs
@@ -1,10 +1,43 @@
 ﻿/**/
+
+Solution s = new Solution();
+TreeNode<int> root = new TreeNode<int>(4);
+root.Insert(2);
+root.Insert(6);
+root.Insert(1);
+root.Insert(3);
+root.Insert(5);
+root.Insert(7);
+
+var result = s.BST(root, 5);    
+if (result != null)
+{
+    Console.WriteLine($"Found: {result.Data}");
+}
+else
+{
+    Console.WriteLine("Node not found");
+}
+
+
 public class Solution
 {
-
-    public TreeNode BST(TreeNode root, int val)
+    //Added ? to the return type of the BST method to indicate that it can return a null value if the node is not found in the binary search tree.
+    public TreeNode<int>? BST(TreeNode<int>? root, int val)
     {
+        if (root == null || root.Data == val)
+        {
+            return root;
+        }
 
+        if (val < root.Data)
+        {
+            return BST(root.Left, val);
+        }
+        else
+        {
+            return BST(root.Right, val);
+        }
     }
 }
 
@@ -24,6 +57,32 @@ public class TreeNode<T> where T : IComparable<T>
     public TreeNode(T data)
     {
         Data = data;
+    }
+
+    public void Insert(T data)
+    {
+        if (data.CompareTo(Data) < 0)
+        {
+            if (Left == null)
+            {
+                Left = new TreeNode<T>(data) { Parent = this };
+            }
+            else
+            {
+                Left.Insert(data);
+            }
+        }
+        else
+        {
+            if (Right == null)
+            {
+                Right = new TreeNode<T>(data) { Parent = this };
+            }
+            else
+            {
+                Right.Insert(data);
+            }
+        }
     }
 
 }
