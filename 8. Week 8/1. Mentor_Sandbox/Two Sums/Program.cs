0bc276065b9702560c8f4d1ad2933b60bf45a280
Tim Eckart <tylerdurden_151@yahoo.com>
2026-08-28 11:37:19 -0700

    Done

---
+++ 8. Week 8/1. Mentor_Sandbox/Two Sums/Program.cs
@@ -0,0 +1,34 @@
+﻿// Two Sums
+
+using System.Globalization;
+
+Solution s = new Solution();
+
+int[] result1 = s.TwoSum(new int[] { -4, -1, 0, 3, 10 });
+int target = 9;
+foreach (int i in s.TwoSum(arr, target))
+{
+    Console.WriteLine(i);
+};
+
+class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int missing = target - nums[i];
+            if (map.ContainsKey(missing))
+            {
+                return new int[] { map[missing], i };
+            }
+            else
+            {
+                int keyValue = nums[i];
+                map[keyValue] = i;
+            }
+        }
+        return new int[] { };
+}
+}
