dfc5cac87344c538da5a06db480d41a00140acd1
Tim Eckart <tylerdurden_151@yahoo.com>
2026-09-10 08:59:25 -0700

    Assignment Completed

---
+++ 00. Weekly Challenge Labs/Week 10 Challenge Labs/Week 10 Challenge Lab 1/Program.cs
@@ -0,0 +1,34 @@
+﻿/**/
+
+Solution result = new Solution();
+Console.WriteLine(result.FindTheDifference("abcd", "abcde"));
+
+public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        //handle edge cases
+        //frequency map
+        int[] letterMap = new int[26];
+        // Count letters from s
+        for (int i = 0; i < s.Length; i++)
+        {
+            letterMap[s[i] - 'a']++;
+        }
+
+        // Remove letters found in t
+        for (int n = 0; n < t.Length; n++)
+        {
+            letterMap[t[n] - 'a']--;
+        }
+
+        for (int j = 0; j < letterMap.Length; j++)
+        {
+            if (letterMap[j] != 0)
+            {
+                return (char)('a' + j);
+            }
+        }
+        return '\0';
+    }
+}
