f1b5fe9e06f121564dd3cdf45a1a17d8535cfcfc
Tim Eckart <tylerdurden_151@yahoo.com>
2026-09-14 15:28:09 -0700

    Assignment COmpleted

--- 00. Weekly Challenge Labs/Week 11 Challenge Labs/Week 11 Challenge Lab 1/Program.cs
+++ 00. Weekly Challenge Labs/Week 11 Challenge Labs/Week 11 Challenge Lab 1/Program.cs
@@ -1,1 +1,63 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent,
+with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+
+Output: [0,1,2]
+ */
+
+int[] nums = new int[] { 2, 0, 2, 1, 1, 0 };
+Console.WriteLine(string.Join(", ", nums));
+Solution result = new Solution();
+
+result.SortColors(nums);
+Console.WriteLine(string.Join(", ", nums));
+
+
+public class Solution
+{
+    public void SortColors(int[] nums)
+    {
+        int low = 0;
+        int mid = 0;
+        int high = nums.Length - 1;
+
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+}
