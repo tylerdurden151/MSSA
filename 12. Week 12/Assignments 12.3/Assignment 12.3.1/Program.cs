169257169367d4986d19900ebf95fb5b335c26e0
Tim Eckart <tylerdurden_151@yahoo.com>
2026-09-23 11:28:32 -0700

    Completed Assignment

--- 12. Week 12/Assignments 12.3/Assignment 12.3.1/Program.cs
+++ 12. Week 12/Assignments 12.3/Assignment 12.3.1/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+ 
+ */
+
+Solution result = new Solution();
+Console.WriteLine(result.RemoveDuplicates("abbaca")); // Output: "ca"
+
+
+public class Solution
+{
+    public string RemoveDuplicates(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (stack.Count > 0 && stack.Peek() == c)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+
+
+        char[] result = new char[stack.Count];
+
+        for (int i = result.Length - 1; i >= 0; i--)
+        {
+            result[i] = stack.Pop();
+        }
+
+        return new string(result);
+    }
+}
